38266dcec3c45f01056e9739de744c09186432cc
Mike Stunes <mikestunes@microsoft.com>
2024-10-18 11:42:44 -0700

    Fix up formatting

--- openhcl/hcl/src/ioctl.rs
+++ openhcl/hcl/src/ioctl.rs
@@ -1842,7 +1842,6 @@ impl<T: Backing> ProcessorRunner<'_, T> {
     ///
     /// # Panics
     /// Panics if `names.len() != values.len()`.
-
     pub fn get_vp_registers(
         &mut self,
         #[cfg(guest_arch = "x86_64")] names: &[HvX64RegisterName],
