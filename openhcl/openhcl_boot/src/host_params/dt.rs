e38fd87152bc51cc64ac1f1bde65c531ae84f6c3
Chris Oo <cho@microsoft.com>
2025-05-30 02:13:34 +0000

    report host provided cmdline to usermode via dt

--- openhcl/openhcl_boot/src/host_params/dt.rs
+++ openhcl/openhcl_boot/src/host_params/dt.rs
@@ -350,6 +350,7 @@ impl PartitionInfo {
         )
         .map_err(|_| DtError::CommandLineSize)?;
 
+        // COMMENT ME OUT / DECIDE WHAT TO DO
         // Depending on policy, write what the host specified in the chosen node.
         if can_trust_host && command_line.policy == CommandLinePolicy::APPEND_CHOSEN {
             // Parse in extra options from the host provided command line.
@@ -357,6 +358,10 @@ impl PartitionInfo {
             write!(storage.cmdline, " {}", &parsed.command_line)
                 .map_err(|_| DtError::CommandLineSize)?;
         }
+
+        // Write the host provided command line.
+        write!(storage.host_provided_cmdline, "{}", parsed.command_line)
+            .map_err(|_| DtError::CommandLineSize)?;
 
         // TODO: Decide if isolated guests always use VTL2 allocation mode.
 
@@ -523,6 +528,7 @@ impl PartitionInfo {
             vmbus_vtl0: _,
             vmbus_vtl2: _,
             cmdline: _,
+            host_provided_cmdline: _,
             com3_serial_available: com3_serial,
             gic,
             memory_allocation_mode: _,
